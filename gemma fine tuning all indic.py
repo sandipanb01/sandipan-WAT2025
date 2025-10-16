d15128d01b37230b579c1750abdefb5a0c5007ec
sandipanb01 <sandipanb12@gmail.com>
2025-10-16 22:24:02 +0530

    Update gemma fine tuning all indic.py

--- gemma fine tuning all indic.py
+++ gemma fine tuning all indic.py
@@ -215,7 +215,7 @@ def evaluate_model(model, tok, max_new_tokens=256, max_samples_per_split=None, b
     for d in preds:
         if not preds[d]: continue
         bleu_scores[d] = sacrebleu.corpus_bleu(preds[d],[refs[d]]).score
-        chrf_scores[d] = sacrebleu.corpus_chrf(preds[d],[refs[d]]).score  
+        chrf_scores[d] = sacrebleu.corpus_chrf(preds[d], [[r] for r in refs[d]]).score 
         comet_scores[d] = comet.compute(
             predictions=preds[d],
             references=refs[d],
