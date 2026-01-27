8e37784ec964a89ec01e2fbd3951c6371a798e04
sandipanb01 <sandipanb12@gmail.com>
2026-01-27 14:47:47 +0530

    Refactor package installation commands in Env.py
    Updated package installation commands in Env.py to use the latest versions without specific version constraints.

--- Env.py
+++ Env.py
@@ -1,15 +1,15 @@
 !pip uninstall -y transformers tokenizers torchvision trl
 !pip install -U \
-    transformers==4.57.3 \
-    tokenizers==0.22.2 \
-    datasets==3.5.0 \
-    sacrebleu==2.5.1 \
-    torch==2.6.0 \
-    torchvision==0.21.0 \
-    tqdm==4.66.5 \
-    peft==0.13.2 \
+    transformers\
+    tokenizers \
+    datasets\
+    sacrebleu\
+    torch\
+    torchvision\
+    tqdm \
+    peft\
     bitsandbytes \
     accelerate
-!pip install trl==0.25.0 #0.12.1
+!pip install trl
 #!pip install git+https://github.com/Unbabel/COMET.git
 !pip install evaluate
