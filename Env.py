17ada9396415d21497805e58e29f191140927f9f
sandipanb01 <sandipanb12@gmail.com>
2026-02-08 14:51:43 +0530

    Update Env.py

--- Env.py
+++ Env.py
@@ -1,15 +1,15 @@
 !pip uninstall -y transformers tokenizers torchvision trl
 !pip install -U \
-    transformers\
-    tokenizers \
-    datasets\
-    sacrebleu\
-    torch\
-    torchvision\
-    tqdm \
-    peft\
+    transformers==4.54.1 \
+    tokenizers==0.21.1 \
+    datasets==3.5.0 \
+    sacrebleu==2.5.1 \
+    torch==2.6.0 \
+    torchvision==0.21.0 \
+    tqdm==4.66.5 \
+    peft==0.13.2 \
     bitsandbytes \
     accelerate
-!pip install trl
+!pip install trl==0.25.0 #0.12.1
 #!pip install git+https://github.com/Unbabel/COMET.git
 !pip install evaluate
