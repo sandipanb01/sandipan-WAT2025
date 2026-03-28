1ef3f698f5a28a683d8ddd85345504f4890a3b3d
sandipanb01 <sandipanb12@gmail.com>
2026-03-28 11:18:04 +0530

    Update train_salesforce_fine-tuning.py

--- train_salesforce_fine-tuning.py
+++ train_salesforce_fine-tuning.py
@@ -134,7 +134,7 @@ tokenizer = AutoTokenizer.from_pretrained(
 )
 
 tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
+#tokenizer.padding_side = "right"
 
 # ============================================================
 # FORMAT EXAMPLES 
@@ -211,7 +211,7 @@ model = AutoModelForCausalLM.from_pretrained(
     torch_dtype=torch.bfloat16,
     attn_implementation="sdpa"
 )
-
+model.config.use_cache = False
 # ============================================================
 # LORA CONFIG
 # ============================================================
@@ -244,6 +244,7 @@ lora_config = LoraConfig(
 training_args = SFTConfig(
 
     output_dir=OUTPUT_DIR,
+    dataset_text_field=None,
 
     per_device_train_batch_size=2,
     gradient_accumulation_steps=16,
